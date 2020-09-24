43f2ee428fafc58cf2f3003d09eeb4c35199f2a3
kupeck <70723820+kupeck@users.noreply.github.com>
2020-09-24 21:19:10 +0200

    E

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 /* Místo " používej ' */
+const logo = document.querySelectorAll("#logo path");
+
+for(let i = 0; i < logo.length; i++) {
+    console.log(`Letter ${i} is ${logo[i].getTotalLength(1)}`);
+}
